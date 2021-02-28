fa4dedd3e6d39c99415cde411031958b7bdc4c43
klauth86 <kehel@yandex.ru>
2021-02-28 11:58:42 +0500

    CPP001 p2

---
+++ CPP001/Source/CPP001/MyAIController.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+UCLASS()
+class CPP001_API AMyAIController : public AAIController {
+
+	GENERATED_UCLASS_BODY()	
+};
---
+++ CPP001/Source/CPP001/MyPathFollowingComponent.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Navigation/PathFollowingComponent.h"
+#include "MyPathFollowingComponent.generated.h"
+
+UCLASS()
+class CPP001_API UMyPathFollowingComponent : public UPathFollowingComponent {
+
+	GENERATED_BODY()
+	
+};
