157aeab76457713d05deb2715c2d287ca0941bce
klauth86 <kehel@yandex.ru>
2021-03-06 17:44:58 +0500

    CPP004 p3 Fixes p1

--- CPP004/Source/CPP004/MyActor.cpp
+++ CPP004/Source/CPP004/MyActor.cpp
@@ -4,35 +4,13 @@
 #include "MyActor.h"
 #include "Engine/World.h"
 
-// Sets default values
-AMyActor::AMyActor()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AMyActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AMyActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 void AMyActor::Test1() {
-	if (Test1Actor) {
-		int i = 10;
-		while (i-- > 0) {
-			auto offset = 400 * (FVector2D::UnitVector + FMath::RandPointInCircle(1));
-			auto location = FVector(offset, 0) + GetActorLocation();
-			GetWorld()->SpawnActor(Test1Actor->GetClass(), &location, &FRotator::ZeroRotator);
-		}
+	if (!Test1Actor) return;
+
+	int i = 10;
+	while (i-- > 0) {
+		auto offset = 400 * (FVector2D::UnitVector + FMath::RandPointInCircle(1));
+		auto location = FVector(offset, 0) + GetActorLocation();
+		GetWorld()->SpawnActor(Test1Actor->GetClass(), &location, &FRotator::ZeroRotator);
 	}
 }
--- CPP004/Source/CPP004/MyActor.h
+++ CPP004/Source/CPP004/MyActor.h
@@ -7,19 +7,8 @@
 
 UCLASS()
 class CPP004_API AMyActor : public AActor {
+	
 	GENERATED_BODY()
-
-public:
-	// Sets default values for this actor's properties
-	AMyActor();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 protected:
 
