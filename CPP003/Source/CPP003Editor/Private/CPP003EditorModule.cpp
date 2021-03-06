20209c0e1c37348c07b0cf6229b64ae31354be6f
klauth86 <kehel@yandex.ru>
2021-03-06 14:12:00 +0500

    CPP003 p6 Fixes p2

--- CPP003/Source/CPP003Editor/Private/CPP003EditorModule.cpp
+++ CPP003/Source/CPP003Editor/Private/CPP003EditorModule.cpp
@@ -9,11 +9,28 @@ DEFINE_LOG_CATEGORY(LogCPP003Editor);
 #define LOCTEXT_NAMESPACE "FCPP003EditorModule"
 
 void FCPP003EditorModule::StartupModule() {
-	GUnrealEd->RegisterComponentVisualizer(UMyActorComponent::StaticClass()->GetFName(), MakeShareable(new FComponentVisualizer_MyActorComp));
+	RegisterComponentVisualizer(UMyActorComponent::StaticClass()->GetFName(), MakeShareable(new FComponentVisualizer_MyActorComp));
 }
 
 void FCPP003EditorModule::ShutdownModule() {
-	GUnrealEd->UnregisterComponentVisualizer(UMyActorComponent::StaticClass()->GetFName());
+	if (GUnrealEd != NULL) {
+		// Iterate over all class names we registered for
+		for (FName ClassName : RegisteredComponentClassNames) {
+			GUnrealEd->UnregisterComponentVisualizer(ClassName);
+		}
+	}
+}
+
+void FCPP003EditorModule::RegisterComponentVisualizer(FName ComponentClassName, TSharedPtr<FComponentVisualizer> Visualizer) {
+	if (GUnrealEd != NULL) {
+		GUnrealEd->RegisterComponentVisualizer(ComponentClassName, Visualizer);
+	}
+
+	RegisteredComponentClassNames.Add(ComponentClassName);
+
+	if (Visualizer.IsValid()) {
+		Visualizer->OnRegister();
+	}
 }
 
 #undef LOCTEXT_NAMESPACE
--- CPP003/Source/CPP003Editor/Public/CPP003EditorModule.h
+++ CPP003/Source/CPP003Editor/Public/CPP003EditorModule.h
@@ -4,9 +4,19 @@
 
 DECLARE_LOG_CATEGORY_EXTERN(LogCPP003Editor, Log, All);
 
+class FComponentVisualizer;
+
 class FCPP003EditorModule : public IModuleInterface {
 public:
 
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+protected:
+
+	void RegisterComponentVisualizer(FName ComponentClassName, TSharedPtr<FComponentVisualizer> Visualizer);
+
+protected:
+
+	TArray<FName> RegisteredComponentClassNames;
 };
