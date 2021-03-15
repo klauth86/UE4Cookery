9528f8d1ed5df21b28d28d6cb3bf61c630f916c2
klauth86 <kehel@yandex.ru>
2021-03-15 16:01:37 +0500

    Add widgets p3

--- CPP007/Source/CPP007/CPP007GameModeBase.cpp
+++ CPP007/Source/CPP007/CPP007GameModeBase.cpp
@@ -12,28 +12,67 @@ void ACPP007GameModeBase::BeginPlay() {
 
 	auto world = GetWorld();
 
-	if (AddToViewportWidgetClass) {
-		if (auto AddToViewportWidget = CreateWidget<UUserWidget>(world, AddToViewportWidgetClass)) {
+	if (AddToViewportWidgetClass_n10) {
+		if (auto AddToViewportWidget = CreateWidget<UUserWidget>(world, AddToViewportWidgetClass_n10)) {
+			AddToViewportWidget->AddToViewport(-10);
+		}
+	}
+
+	if (AddToViewportWidgetClass_0) {
+		if (auto AddToViewportWidget = CreateWidget<UUserWidget>(world, AddToViewportWidgetClass_0)) {
 			AddToViewportWidget->AddToViewport();
 		}
 	}
 
+	if (AddToViewportWidgetClass_p10) {
+		if (auto AddToViewportWidget = CreateWidget<UUserWidget>(world, AddToViewportWidgetClass_p10)) {
+			AddToViewportWidget->AddToViewport(10);
+		}
+	}
+
 	auto controller = UGameplayStatics::GetPlayerController(this, 0);
-	if (AddToPlayerScreenWidgetClass && controller) {
-		if (auto AddToPlayerScreenWidget = CreateWidget<UUserWidget>(controller, AddToPlayerScreenWidgetClass)) {
+	if (AddToPlayerScreenWidgetClass_n10 && controller) {
+		if (auto AddToPlayerScreenWidget = CreateWidget<UUserWidget>(controller, AddToPlayerScreenWidgetClass_n10)) {
+			AddToPlayerScreenWidget->AddToPlayerScreen(-10);
+		}
+	}
+
+	if (AddToPlayerScreenWidgetClass_0 && controller) {
+		if (auto AddToPlayerScreenWidget = CreateWidget<UUserWidget>(controller, AddToPlayerScreenWidgetClass_0)) {
 			AddToPlayerScreenWidget->AddToPlayerScreen();
 		}
 	}
 
-	if (LayerManagerWidgetClass && controller) {
-		if (auto localPlayer = controller->GetLocalPlayer()) {
-			if (auto ViewportClient = world->GetGameViewport()) {
-				if (auto manager = ViewportClient->GetGameLayerManager()) {
-					
-					LayerManagerWidget = CreateWidget<UUserWidget>(controller, LayerManagerWidgetClass);
-					if (LayerManagerWidget) {						
-						manager->AddWidgetForPlayer(localPlayer, LayerManagerWidget->TakeWidget(), 0);
-					}
+	if (AddToPlayerScreenWidgetClass_p10 && controller) {
+		if (auto AddToPlayerScreenWidget = CreateWidget<UUserWidget>(controller, AddToPlayerScreenWidgetClass_p10)) {
+			AddToPlayerScreenWidget->AddToPlayerScreen(10);
+		}
+	}
+
+	auto localPlayer = controller->GetLocalPlayer();
+	auto ViewportClient = world->GetGameViewport();
+
+	if (localPlayer && ViewportClient) {
+		if (auto manager = ViewportClient->GetGameLayerManager()) {
+
+			if (LayerManagerWidgetClass_n10 && controller) {
+				LayerManagerWidget_n10 = CreateWidget<UUserWidget>(controller, LayerManagerWidgetClass_n10);
+				if (LayerManagerWidget_n10) {
+					manager->AddWidgetForPlayer(localPlayer, LayerManagerWidget_n10->TakeWidget(), 0);
+				}
+			}
+
+			if (LayerManagerWidgetClass_0 && controller) {
+				LayerManagerWidget_0 = CreateWidget<UUserWidget>(controller, LayerManagerWidgetClass_0);
+				if (LayerManagerWidget_0) {
+					manager->AddWidgetForPlayer(localPlayer, LayerManagerWidget_0->TakeWidget(), 0);
+				}
+			}
+
+			if (LayerManagerWidgetClass_p10 && controller) {
+				LayerManagerWidget_p10 = CreateWidget<UUserWidget>(controller, LayerManagerWidgetClass_p10);
+				if (LayerManagerWidget_p10) {
+					manager->AddWidgetForPlayer(localPlayer, LayerManagerWidget_p10->TakeWidget(), 0);
 				}
 			}
 		}
--- CPP007/Source/CPP007/CPP007GameModeBase.h
+++ CPP007/Source/CPP007/CPP007GameModeBase.h
@@ -20,14 +20,38 @@ protected:
 	virtual void BeginPlay();
 
 	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
-		TSubclassOf<UUserWidget> AddToViewportWidgetClass;
+		TSubclassOf<UUserWidget> AddToViewportWidgetClass_n10;
 
 	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
-		TSubclassOf<UUserWidget> AddToPlayerScreenWidgetClass;
+		TSubclassOf<UUserWidget> AddToViewportWidgetClass_0;
 
 	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
-		TSubclassOf<UUserWidget> LayerManagerWidgetClass;
+		TSubclassOf<UUserWidget> AddToViewportWidgetClass_p10;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> AddToPlayerScreenWidgetClass_n10;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> AddToPlayerScreenWidgetClass_0;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> AddToPlayerScreenWidgetClass_p10;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> LayerManagerWidgetClass_n10;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> LayerManagerWidgetClass_0;
+
+	UPROPERTY(EditDefaultsOnly, category = "CPP007 Game Mode")
+		TSubclassOf<UUserWidget> LayerManagerWidgetClass_p10;
 
 	UPROPERTY()
-		UUserWidget* LayerManagerWidget;
+		UUserWidget* LayerManagerWidget_n10;
+
+	UPROPERTY()
+		UUserWidget* LayerManagerWidget_0;
+
+	UPROPERTY()
+		UUserWidget* LayerManagerWidget_p10;
 };
