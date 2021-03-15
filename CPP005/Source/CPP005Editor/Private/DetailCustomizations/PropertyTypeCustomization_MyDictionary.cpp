6653cf1227a213c8d337c0e29fecd90726c66aa1
klauth86 <kehel@yandex.ru>
2021-03-15 09:14:25 +0500

    CPP005 p6 Remove unused, add dependencies and widget logic

--- CPP005/Source/CPP005Editor/Private/DetailCustomizations/PropertyTypeCustomization_MyDictionary.cpp
+++ CPP005/Source/CPP005Editor/Private/DetailCustomizations/PropertyTypeCustomization_MyDictionary.cpp
@@ -1,4 +1,9 @@
 #include "PropertyTypeCustomization_MyDictionary.h"
+#include "DetailCategoryBuilder.h"
+#include "DetailWidgetRow.h"
+#include "SGraphPinComboBox.h"
+#include "IPropertyUtilities.h"
+#include "MyDictionary.h"
 
 #define LOCTEXT_NAMESPACE "PropertyTypeCustomization_MyDictionary"
 
@@ -8,6 +13,67 @@ TSharedRef<IPropertyTypeCustomization> FPropertyTypeCustomization_MyDictionary::
 
 void FPropertyTypeCustomization_MyDictionary::CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) {
 
+	PropertyHandlePtr = PropertyHandle;
+	TSharedPtr<IPropertyUtilities> PropertyUtils = CustomizationUtils.GetPropertyUtilities();
+
+	// Get list of MyDictionary indexes
+	TArray< TSharedPtr<int32> > ComboItems;
+	GenerateComboBoxIndexes(ComboItems);
+
+	SAssignNew(ComboBox, SPinComboBox)
+		.ComboItemList(ComboItems)
+		.VisibleText(this, &FPropertyTypeCustomization_MyDictionary::OnGetText)
+		.OnSelectionChanged(this, &FPropertyTypeCustomization_MyDictionary::ComboBoxSelectionChanged)
+		.OnGetDisplayName(this, &FPropertyTypeCustomization_MyDictionary::OnGetFriendlyName)
+		.OnGetTooltip(this, &FPropertyTypeCustomization_MyDictionary::OnGetTooltip);
+
+	HeaderRow.NameContent()[PropertyHandle->CreatePropertyNameWidget()]
+		.ValueContent()[ComboBox.ToSharedRef()].IsEnabled(MakeAttributeLambda([=] { return !PropertyHandle->IsEditConst() && PropertyUtils->IsPropertyEditingEnabled(); }));
+}
+
+void FPropertyTypeCustomization_MyDictionary::GenerateComboBoxIndexes(TArray< TSharedPtr<int32> >& OutComboBoxIndexes) {
+	int32 i = 0;
+	for (auto item : FMyDictionary::Items) {
+		TSharedPtr<int32> EnumIdxPtr(new int32(i++));
+		OutComboBoxIndexes.Add(EnumIdxPtr);
+	}
+}
+
+FString FPropertyTypeCustomization_MyDictionary::OnGetText() const {
+
+	if (auto myDictionary = GetPropertyAs<FMyDictionary>()) {
+
+		auto itemIndex = myDictionary->ItemIndex;
+		
+		return (FMyDictionary::Items.Num() < itemIndex || itemIndex < 0)
+			? ""
+			: FMyDictionary::Items[itemIndex].ToString();
+	}
+
+	return "";
+}
+
+void FPropertyTypeCustomization_MyDictionary::ComboBoxSelectionChanged(TSharedPtr<int32> NewSelection, ESelectInfo::Type /*SelectInfo*/) {
+	//if (NewSelection.IsValid()) {
+
+	//	FStructProperty* StructProperty = CastField<FStructProperty>(StructPropertyHandle->GetProperty());
+
+	//	if (auto replica = GetPropertyAs<FReplica>()) {
+	//		replica->Index = *NewSelection;
+	//	}
+	//}
+}
+
+FText FPropertyTypeCustomization_MyDictionary::OnGetFriendlyName(int32 itemIndex) {
+	return (FMyDictionary::Items.Num() < itemIndex || itemIndex < 0)
+		? FText::GetEmpty()
+		: FText::FromName(FMyDictionary::Items[itemIndex]);
+}
+
+FText FPropertyTypeCustomization_MyDictionary::OnGetTooltip(int32 itemIndex) {
+	return (FMyDictionary::Items.Num() < itemIndex || itemIndex < 0)
+		? FText::GetEmpty()
+		: FText::FromName(FMyDictionary::Items[itemIndex]);
 }
 
 #undef LOCTEXT_NAMESPACE
--- CPP005/Source/CPP005Editor/Private/DetailCustomizations/PropertyTypeCustomization_MyDictionary.h
+++ CPP005/Source/CPP005Editor/Private/DetailCustomizations/PropertyTypeCustomization_MyDictionary.h
@@ -2,6 +2,8 @@
 
 #include "IPropertyTypeCustomization.h"
 
+class SPinComboBox;
+
 class CPP005EDITOR_API FPropertyTypeCustomization_MyDictionary : public IPropertyTypeCustomization {
 
 public:
@@ -11,4 +13,33 @@ public:
 	void CustomizeHeader(TSharedRef<IPropertyHandle> PropertyHandle, FDetailWidgetRow& HeaderRow, IPropertyTypeCustomizationUtils& CustomizationUtils) override;
 
 	virtual void CustomizeChildren(TSharedRef<class IPropertyHandle> StructPropertyHandle, class IDetailChildrenBuilder& StructBuilder, IPropertyTypeCustomizationUtils& StructCustomizationUtils) override {}
+
+protected:
+
+	void GenerateComboBoxIndexes(TArray< TSharedPtr<int32> >& OutComboBoxIndexes);
+
+	FString OnGetText() const;
+
+	void ComboBoxSelectionChanged(TSharedPtr<int32> NewSelection, ESelectInfo::Type SelectInfo);
+
+	FText OnGetFriendlyName(int32 itemIndex);
+
+	FText OnGetTooltip(int32 itemIndex);
+
+	template<typename T>
+	T* GetPropertyAs() const {
+		if (PropertyHandlePtr.IsValid()) {
+			TArray<void*> RawData;
+			PropertyHandlePtr->AccessRawData(RawData);
+			return reinterpret_cast<T*>(RawData[0]);
+		}
+
+		return nullptr;
+	}
+
+protected:
+
+	TSharedPtr<IPropertyHandle> PropertyHandlePtr;
+
+	TSharedPtr<SPinComboBox> ComboBox;
 };
