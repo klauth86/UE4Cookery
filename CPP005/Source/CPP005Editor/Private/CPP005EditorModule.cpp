0459f1c193ab760433b57eab178c066f299150c5
klauth86 <kehel@yandex.ru>
2021-03-15 09:22:07 +0500

    CPP005 p7 Add register code to Editor module

--- CPP005/Source/CPP005Editor/Private/CPP005EditorModule.cpp
+++ CPP005/Source/CPP005Editor/Private/CPP005EditorModule.cpp
@@ -1,15 +1,25 @@
 #include "CPP005EditorModule.h"
+#include "PropertyEditorModule.h"
+#include "MyDictionary.h"
+#include "DetailCustomizations/PropertyTypeCustomization_MyDictionary.h"
 
 DEFINE_LOG_CATEGORY(LogCPP005Editor);
 
 #define LOCTEXT_NAMESPACE "FCPP005EditorModule"
 
 void FCPP005EditorModule::StartupModule() {
-	
+	// Register the details customizer
+	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	PropertyModule.RegisterCustomPropertyTypeLayout(FMyDictionary::StaticStruct()->GetFName(), 
+		FOnGetPropertyTypeCustomizationInstance::CreateStatic(&FPropertyTypeCustomization_MyDictionary::MakeInstance));
 }
 
 void FCPP005EditorModule::ShutdownModule() {
-	
+	// Unregister the details customization
+	if (FModuleManager::Get().IsModuleLoaded("PropertyEditor")) {
+		FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+		PropertyModule.UnregisterCustomPropertyTypeLayout(FMyDictionary::StaticStruct()->GetFName());
+	}
 }
 
 #undef LOCTEXT_NAMESPACE
