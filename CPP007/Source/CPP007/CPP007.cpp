ce3cd5a10d27aeeb6b34795e7c99f32b9cf5ee5d
klauth86 <kehel@yandex.ru>
2021-03-15 13:54:43 +0500

    CPP007 Add project

---
+++ CPP007/Source/CPP007/CPP007.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPP007.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP007, "CPP007" );
---
+++ CPP007/Source/CPP007/CPP007GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP007GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP007_API ACPP007GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
