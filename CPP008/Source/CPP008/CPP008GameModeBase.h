901b6c6af0836aa8f0f7aecc35e833ff683076ee
klauth86 <kehel@yandex.ru>
2021-06-02 14:12:47 +0500

    CPP008 Add project

---
+++ CPP008/Source/CPP008/CPP008GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP008GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP008_API ACPP008GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
