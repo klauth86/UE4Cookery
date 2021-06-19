5a7eeeb9b3052b03d15e0a0752b345de8ec38bab
klauth86 <kehel@yandex.ru>
2021-06-19 18:12:17 +0500

    CPP009 Add Editor module

--- CPP009/Source/CPP009Editor.Target.cs
+++ CPP009/Source/CPP009Editor.Target.cs
@@ -9,6 +9,6 @@ public class CPP009EditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "CPP009" } );
+		ExtraModuleNames.AddRange( new string[] { "CPP009Editor" } );
 	}
 }
---
+++ CPP009/Source/CPP009Editor/Private/CPP009EditorModule.cpp
@@ -0,0 +1,17 @@
+#include "CPP009EditorModule.h"
+
+DEFINE_LOG_CATEGORY(LogCPP009Editor);
+
+#define LOCTEXT_NAMESPACE "FCPP009EditorModule"
+
+void FCPP009EditorModule::StartupModule() {
+	
+}
+
+void FCPP009EditorModule::ShutdownModule() {
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCPP009EditorModule, CPP009Editor);
