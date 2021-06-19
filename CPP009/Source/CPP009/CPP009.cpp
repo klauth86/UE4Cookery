7388289828e005a3c53672679d84ed380a74b52d
klauth86 <kehel@yandex.ru>
2021-06-19 18:01:23 +0500

    CPP009 Add project

---
+++ CPP009/Source/CPP009/CPP009.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPP009.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP009, "CPP009" );
