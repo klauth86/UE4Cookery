facd71a26487f89a4dc1bea31af7712026061aff
klauth86 <kehel@yandex.ru>
2021-10-26 20:43:22 +0500

    CPP012 Add PlayerController subclass

---
+++ CPP012/Source/CPP012/MyPlayerController.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+UCLASS()
+class CPP012_API AMyPlayerController : public APlayerController {
+
+	GENERATED_UCLASS_BODY()
+};
