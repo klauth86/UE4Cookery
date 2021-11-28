27e3cf2bc9ab82e04259b3504da51eda99911bd0
klauth86 <kehel@yandex.ru>
2021-11-28 20:25:47 +0500

    Remove unused

--- CPP012/Source/CPP012/SMyVirtualJoystick.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SMyVirtualJoystick.h"
-
-void SMyVirtualJoystick::Construct(const FArguments& InArgs) {
-
-}
